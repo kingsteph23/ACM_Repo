e7b8c650d3d7c373d643b8e2e680e5dfbf2e4b33
kingsteph23 <philippeaustephen@gmail.com>
2024-09-12 23:46:32 -0400

    hehe-not stephen

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hey");
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Hello index" + i);
+            System.out.println("wat happens if i dont");
+
         }
     }
 }
